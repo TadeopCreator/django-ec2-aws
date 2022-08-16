2d7b58dc5108f61e58dfeed8137fc70b4fd03bf2
Tadeo Deluca <tadeo.deluca2002@gmail.com>
2022-08-16 03:32:32 -0300

    string changed

--- django_aws/settings.py
+++ django_aws/settings.py
@@ -19,8 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+import os
+
+os.environ
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-s4#_!8g*e0tjt)1z2%5)09d7@3&+&6fte=w*!vu82sp=9i^4n9"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
