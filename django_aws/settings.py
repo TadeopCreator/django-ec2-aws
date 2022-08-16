93702467eb0f388b9732581620042c5788b077d5
Tadeo Deluca <tadeo.deluca2002@gmail.com>
2022-08-16 03:49:06 -0300

    added .env

--- django_aws/settings.py
+++ django_aws/settings.py
@@ -19,12 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+import environ
 import os
-
-os.environ
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
